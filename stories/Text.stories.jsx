e95ede672e4d3a19b4e8592d1c16ee28ebaa6cdf
oreqizer <oreqizer@gmail.com>
2018-07-12 12:09:28 +0200

    general: Cleanup

--- stories/Text.stories.jsx
+++ stories/Text.stories.jsx
@@ -1,7 +1,7 @@
 // @flow strict
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { withKnobs, text } from "@storybook/addon-knobs";
+import { withKnobs, text } from "@storybook/addon-knobs/react";
 
 import Text from "client/components/Text";
 
