29260cb2dd665f968a0c7b302f8850e77ef6fc0c
oreqizer <oreqizer@gmail.com>
2018-11-23 11:30:59 +0100

    server: Remove redundant console.log

--- src/server/app.js
+++ src/server/app.js
@@ -7,7 +7,6 @@ import markup from "./markup";
 async function app(ctx: Context) {
   const [theme = "main", locale = "en"] = ctx.path.slice(1).split("/"); // /<theme>/<locale>
 
-  console.log(theme, locale)
   ctx.status = 200;
   ctx.type = "text/html; charset=utf-8";
   ctx.body = markup(ctx.url, theme, locale);
