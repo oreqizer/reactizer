0433c04844679497463b6095ec0db7341ea6a1f6
Martin Olsansky <olso@users.noreply.github.com>
2019-10-28 12:03:04 +0100

    Update app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,22 @@
 /* eslint-disable no-param-reassign */
 import { Context } from "koa";
+import { parse } from "url";
 
 import markup from "./markup";
 
-async function app(ctx: Context) {
+async function app(ctx: Context, next: () => Promise<void>) {
+  const urlObj = parse(ctx.url);
+
+  // skip 404s
+  if (urlObj.pathname && urlObj.pathname.match(/\.(jpe?g|png|gif|bmp|svg|ico|js|json|txt|css)$/i)) {
+    return next();
+  }
+
   ctx.status = 200;
   ctx.type = "text/html; charset=utf-8";
   ctx.body = markup(ctx.url, "main", "en-GB");
+
+  return next();
 }
 
 export default app;
