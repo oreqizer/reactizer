bf1d1d8ee5c5314a8dc9c00106e88953285191cd
oreqizer <oreqizer@gmail.com>
2020-08-20 10:19:44 +0200

    fix(webpack): extensions 2

--- tools/webpack/index.js
+++ tools/webpack/index.js
@@ -6,7 +6,7 @@ const dev = require("./parts/dev");
 
 const common = (mode) => ({
   resolve: {
-    extensions: [".ts", ".tsx", ".esm.js", ".mjs", ".js"],
+    extensions: [".ts", ".tsx", ".mjs", ".js"],
   },
   plugins: [
     new LoadablePlugin({
