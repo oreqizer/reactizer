ccddf56f14d77bd0bf9e944e47f5b1c4008a367d
oreqizer <oreqizer@gmail.com>
2020-09-04 14:02:26 +0200

    feat(meta): add missing deps

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 
-const boilerplates = fs.readdirSync("boilerplates");
 const packages = fs.readdirSync("packages");
 const tools = fs.readdirSync("tools");
 
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["meta", ...boilerplates, ...packages, ...tools]],
+    "scope-enum": [2, "always", ["meta", ...packages, ...tools]],
   },
 };
