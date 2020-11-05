443a67135ececeab4293c7127bddb736436c480a
oreqizer <oreqizer@gmail.com>
2020-11-05 13:01:11 +0100

    fix(webpack): cleanup

--- packages/webpack/parts/dev.js
+++ packages/webpack/parts/dev.js
@@ -1,17 +1,14 @@
 module.exports = {
   output: {
     publicPath: "/",
-    filename: "[name].js",
   },
   devtool: "cheap-module-source-map",
   devServer: {
     proxy: {
       "*": "http://localhost:3000",
     },
-    // host: "0.0.0.0", // if you want to expose it on your LAN
     inline: true,
     hot: true,
     progress: true,
-    quiet: true,
   },
 };
