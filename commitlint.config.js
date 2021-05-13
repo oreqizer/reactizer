4ce03d96f7b3b43d84750b3ebef7605ea07240d8
oreqizer <oreqizer@gmail.com>
2021-05-13 13:45:24 +0200

    feat(dev): upgrade deps

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,6 @@ const packages = fs.readdirSync("packages");
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["meta", ...packages]],
+    "scope-enum": [2, "always", ["dev", ...packages]],
   },
 };
